e2bcb4603a10f86256ae109ece98d62a78fcb2fb
Георгий Орлов <orlov1994@bk.ru>
2023-06-20 22:49:08 +0300

    Добавьте файлы проекта.

---
+++ Strategy/LogProcessor.cs
@@ -0,0 +1,47 @@
+﻿/// пример Стратегии для сохранения логов
+
+namespace Strategy;
+
+/// <summary>
+/// Модель логов
+/// </summary>
+internal class LogEntry
+{
+}
+
+/// <summary>
+/// на диаграмме это "Контекст"
+/// </summary>
+internal class LogProcessor
+{
+    /// <summary>
+    /// вместо ILogReader, сюда помещается метод из конкретной стратегии, чтобы не делать
+    /// интерфейс с 1-им методом. На диаграмме - это "Стратегия"
+    /// </summary>
+    private readonly Func<List<LogEntry>> _logImporter;
+
+    /// <summary>
+    /// конструктор для определения стратегии, вместо интерфейса - делегат
+    /// </summary>
+    /// <param name="logImporter">конкретная реализация стратегии</param>
+    public LogProcessor(Func<List<LogEntry>> logImporter)
+    {
+        _logImporter = logImporter;
+    }
+
+    /// <summary>
+    /// пример использования сохранения логов
+    /// </summary>
+    public void ProcessLogs()
+    {
+        foreach (var logEntry in _logImporter.Invoke())
+        {
+            SaveLogEntry(logEntry);
+        }
+    }
+
+    private void SaveLogEntry(LogEntry logEntry)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Strategy/SortLists.cs
@@ -0,0 +1,27 @@
+﻿// Пример стратегии для сортировки
+
+namespace Strategy;
+
+internal class Employee
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public override string ToString() => string.Format("Id = {0}, Name = {1}", Id, Name);
+}
+
+internal class EmployeeByIdComparer : IComparer<Employee>
+{
+    public int Compare(Employee? x, Employee? y)
+    {
+        return x.Id.CompareTo(y.Id);
+    }
+}
+
+public static class SortLists
+{
+    public static void Sort()
+    {
+        var list = new List<Employee>();
+    }
+}
